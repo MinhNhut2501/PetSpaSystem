452b18347136652e28ada5ebb35aa227e262cb78
nhuttranaffina <nhut.tran@affina.com.vn>
2025-06-19 10:29:59 +0700

    push service

---
+++ notification-service/notification-service/src/test/java/com/petspa/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petspa.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
