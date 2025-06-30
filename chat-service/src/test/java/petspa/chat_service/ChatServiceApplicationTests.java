2cd0b485a6f3ababf5680087748592c1dd7ea36e
nhuttranaffina <nhut.tran@affina.com.vn>
2025-06-30 14:31:33 +0700

    add feature v3

---
+++ chat-service/src/test/java/petspa/chat_service/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package petspa.chat_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
